78fea9b4097c866144e3578381515ac2935b48df
Lieety <lieety10.07@gmail.com>
2024-08-25 13:23:51 +0200

    sdf

---
+++ actions/auth-action.ts
@@ -0,0 +1,24 @@
+"use server";
+
+import { loginSchema } from "@/lib/zod";
+import { signIn } from "@/auth";
+import { z } from "zod";
+import { AuthError } from "next-auth";
+
+export const loginAction = async(
+    values: z.infer<typeof loginSchema>
+) => {
+    try{
+        await signIn("credentials", {
+            email: values.email,
+            password: values.password,
+            redirect: false,
+          })
+      return { succes: true };    
+    } catch (error){
+        if (error instanceof AuthError) {
+            return {error: error.cause?.err?.message};
+        }
+        return {error: "·error 500"}
+    }
+}
--- auth.ts
+++ auth.ts
@@ -1,13 +1,12 @@
-import NextAuth from 'next-auth'
-import authConfig from './auth.config'
+import NextAuth from "next-auth";
 
-import { PrismaClient } from '@prisma/client'
-import { PrismaAdapter } from '@auth/prisma-adapter'
+import { PrismaAdapter } from "@auth/prisma-adapter";
 
-const prisma = new PrismaClient()
-
-export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: PrismaAdapter(prisma),
-  session: { strategy: 'jwt' },
-  ...authConfig
+import authConfig from "./auth.config";
+import { db } from "@/lib/db";
+ 
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(db),
+  ...authConfig,
+  session: { strategy: "jwt" },
 })
